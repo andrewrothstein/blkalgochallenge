5275cc3bd45f2e137bb8d07b369915a35b08ac54
Andrew Rothstein <drew@drewfus.(none)>
2013-03-09 23:34:56 -0500

    more work on challenge 5

--- project/BlkalgochallengesBuild.scala
+++ project/BlkalgochallengesBuild.scala
@@ -12,6 +12,9 @@ object BlkalgochallengesBuild extends Build {
       version := "0.1-SNAPSHOT",
       scalaVersion := "2.10.0"
       // add other settings here
+//     , libraryDependencies ++= Seq("com.typesafe" %% "scalalogging-slf4j" % "1.0.1")
     )
   )
+
+  
 }
--- src/main/scala/com/bfm/C5.scala
+++ src/main/scala/com/bfm/C5.scala
@@ -1,22 +1,44 @@
 package com.bfm
 
-case class Position(totalPositions :Int, pos :Int, antCount :Int) {
-  val step = if (pos == antCount || pos == antCount || Prime.is(pos)) -1 else 1
+case class Ant(totalPos :Int, pos :Int, speed :Int) {
 
-  def next = {
-    if (pos + step < 0) { 
-      Position(totalPositions, totalPositions - (pos + step), antCount)
-    } else if (pos + step >= totalPositions) {
-       Position(totalPositions, (pos + step) - totalPositions, antCount)
-    } else {
-      Position(totalPositions, pos + step, antCount)
-    }
+  def speedUp = Ant(totalPos, pos, 1 + speed)
+
+  def effStep(s :Int) :Int = if (s < 0) {
+    effStep(s + totalPos)
+  } else if (s >= totalPos) {
+    effStep(s - totalPos) 
+  } else s
+
+  def step = {
+    val s = effStep(if (pos != 0 && pos != 1 && Prime.is(pos)) -speed else speed)
+    val nextPos = (pos + s) % totalPos
+    Ant(totalPos, nextPos, speed)
   }
 }
 
-case class Table(totalPositions :Int) {
-  def agg(positions :List[Position])
+case class Ring(ants :Seq[Ant]) {
+
+  def this(totalPos :Int) = this(for (p <- Range(0, totalPos)) yield Ant(totalPos, p, 1))
+
+  def step = {
+    val nextAnts = ants map (_.step)
+    val groupedByPos = nextAnts groupBy (a => a.pos)
+    val (toSpeedUp, noSpeedUp) = groupedByPos partition (s => s._2.size > 1)
+    val spedUp = toSpeedUp flatMap (a => a._2.map(_.speedUp))
+    val notSpedUp = noSpeedUp flatMap (a => a._2)
+    Ring((spedUp.toList ::: notSpedUp.toList) sortWith ((x, y) => (x.pos < y.pos)))
+  }
+
+  def isBalanced = ants groupBy (a => a.pos) forall (s => s._2.size == 1)
+}
 
 object C5 extends App {
-  println("Hello, blkalgochallenges")
+  var r = new Ring(5)
+  var cnt = 0;
+  do {
+    r = r.step; 
+    cnt = cnt + 1
+    println(cnt + ": " + r)
+  } while (!r.isBalanced && cnt < 100)
 }
